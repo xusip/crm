1894f7fb66b68a2a8eb476d8f3dddd2f50e49f9c
xsp <xsp@163.com>
2023-03-21 21:12:36 +0800

    second commit

---
+++ crm/src/test/java/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * @program: CRM
+ * @description:
+ * @author: xsp
+ * @create: 2023-03-21 21:09
+ * @version:1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2222!");
+    }
+}
